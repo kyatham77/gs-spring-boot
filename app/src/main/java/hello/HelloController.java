9c5008b2b6facbfb3f322be712f7f235f13a8fbd
kyatham77 <41563687+kyatham77@users.noreply.github.com>
2019-06-15 11:31:01 -0400

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Hello from Srini!";
     }   
 }
